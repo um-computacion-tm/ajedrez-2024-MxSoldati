0248f13f6afc6602317e600b81c3511c5b0d42eb
MxSoldati <m.soldati@alumno.um.edu.ar>
2024-09-11 13:54:12 -0300

    2024-09-11

--- game/pieces/pawn.py
+++ game/pieces/pawn.py
@@ -9,7 +9,7 @@ class Pawn(Piece):
         if self.__color__ == "WHITE":
             possible_positions = (
                 self.possible_movement_vertical_down(from_row, from_col) + 
-                self.possible_attack_diagonal_down_left(from_row, from_col) +
+                self.possible_attack_diagonal_down(from_row, from_col) +
                 self.possible_attack_diagonal_down_right(from_row, from_col)
             )
         else:
@@ -20,21 +20,48 @@ class Pawn(Piece):
             )
         return (to_row, to_col) in possible_positions
 
+
+        
+    def possible_movement_vertical_down(self, row, col, start_row=6):
+        return self.possible_movement_vertical(row, col, PiceMovements.movement_vertical_down, start_row)
+    def possible_movement_vertical_up(self, row, col, start_row=1):
+        return self.possible_movement_vertical(row, col, PiceMovements.movement_vertical_up, start_row)
+        
+    def possible_movement_vertical(self, row, col, movement_func, start_row):
+        import ipdb; ipdb.set_trace()
+        if row == start_row and self.__board__.get_piece(row + 2 , col) == None:
+            return movement_func(self, row, col)[:2]
+        elif self.__board__.get_piece(row + 1 , col) == None:
+            return movement_func(self, row, col)[:1]
+        else:
+            return []
+
+
+
+    def possible_attack_diagonal_down_left(self, row, col):
+        return self.possible_attack_diagonal(row, col, PiceMovements.movement_diagonal_down_left)
+    def possible_attack_diagonal_down_right(self, row, col):
+        return self.possible_attack_diagonal(row, col, PiceMovements.movement_diagonal_down_right)
+    
+    def possible_attack_diagonal_down(self, row, col, movement_func):
+        return movement_func(self, row, col)[:1]
+
+
     #Para pawn blanco
-    def possible_movement_vertical_down(self, row, col):
-        if row == 6:
-            return PiceMovements.movement_vertical_down(self, row, col)[:2]
-        else:
-            return PiceMovements.movement_vertical_down(self, row, col)[:1]
+    # def possible_movement_vertical_down(self, row, col):
+    #     if row == 6:
+    #         return PiceMovements.movement_vertical_down(self, row, col)[:2]
+    #     else:
+    #         return PiceMovements.movement_vertical_down(self, row, col)[:1]
 
 
-    #Para pawn negro
+    # #Para pawn negro
 
-    def possible_movement_vertical_up(self, row, col):
-        if row == 1:
-            return PiceMovements.movement_vertical_up(self, row, col)[:2]
-        else:
-            return PiceMovements.movement_vertical_up(self, row, col)[:1]
+    # def possible_movement_vertical_up(self, row, col):
+    #     if row == 1:
+    #         return PiceMovements.movement_vertical_up(self, row, col)[:2]
+    #     else:
+    #         return PiceMovements.movement_vertical_up(self, row, col)[:1]
 
 
     def possible_attack_diagonal_down_left(self, row, col): 
